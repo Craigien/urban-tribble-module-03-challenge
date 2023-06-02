2c9ad374e1313566de13dcd2e52afc6063fbd526
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-06-02 18:11:05 -0400

    Basic password creation functionality completed

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,8 +2,25 @@
 
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbericCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = [];
+var numericCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ["!", "?", "@", "#"];
+
+function getCharacterCount()
+{
+  // Get desired number of characters from user
+  // Need to check for non numeric inputs
+  do
+  {
+    var charCount = window.prompt("How many characters should the password be?");
+
+    // Convert string to number
+    //Number(charCount);
+    
+  }
+  while (charCount < "8" && charCount > "128");
+
+  return charCount;
+}
 
 function getCharacterTypes ()
 {
@@ -49,7 +66,7 @@ function getCharacterTypes ()
     window.alert("You selected no to use special characters");
   }
 
-  return {lowerCaseAnswer, upperCaseAnswer, numericAnswer, specialCharacterAnswer};
+  return [lowerCaseAnswer, upperCaseAnswer, numericAnswer, specialCharacterAnswer];
 }
 
 
@@ -60,20 +77,63 @@ function getCharacterTypes ()
 function generatePassword()
 {
   // Get desired number of characters from user
-  // Need to check for non numeric inputs
-  do
-  {
-    var charCount = window.prompt("How many characters should the password be?");
-
-    // Convert string to number
-    //Number(charCount);
-    
-  }
-  while (charCount < "8" && charCount > "128");
+  var charCount = getCharacterCount();
+  console.log(charCount);
 
   // Call getCharacterTypes function to get Boolean values
   var characterTypesAnswers = getCharacterTypes();
+  console.log(characterTypesAnswers);
 
+  // Generate the password
+  var passwordArray = [];
+
+  for (var i = 0; i < charCount; i++)
+  {
+    //Choose character type
+    if ((characterTypesAnswers[0]) && (characterTypesAnswers[1]) && (characterTypesAnswers[2]) && (characterTypesAnswers[3]))
+    {
+      console.log("True");
+      // Gets random number between 0 and 3
+      var characterType = Math.floor(Math.random() * 4)
+      console.log("Character Type: " + characterType);
+
+      if (characterType === 0)
+      {
+        var characterNumber = Math.floor(Math.random() * upperCaseCharacters.length);
+        var character = upperCaseCharacters[characterNumber];
+        console.log(character);
+        passwordArray.push(character);
+      }
+      else if (characterType === 1)
+      {
+        var characterNumber = Math.floor(Math.random() * lowerCaseCharacters.length);
+        var character = lowerCaseCharacters[characterNumber];
+        console.log(character);
+        passwordArray.push(character);
+      }
+      else if (characterType === 2)
+      {
+        var characterNumber = Math.floor(Math.random() * numericCharacters.length);
+        var character = numericCharacters[characterNumber];
+        console.log(character);
+        passwordArray.push(character);
+      }
+      else
+      {
+        var characterNumber = Math.floor(Math.random() * specialCharacters.length);
+        var character = specialCharacters[characterNumber];
+        console.log(character);
+        passwordArray.push(character);
+      }
+    }
+  }
+
+  var password = passwordArray.join("");
+
+  console.log(passwordArray);
+  console.log(password);
+
+  return password;
 }
 
 
