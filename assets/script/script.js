12d52e8ecaa4d917deea131ce89adadf2241265a
cvon117 <125157011+Craigien@users.noreply.github.com>
2023-06-03 13:13:02 -0400

    Added comments to JavaScript file and created .gitignore file

--- assets/script/script.js
+++ assets/script/script.js
@@ -27,6 +27,7 @@ function getCharacterCount()
 // Get desired character types from user
 function getCharacterTypes ()
 {
+  // Confirm with user which character types to use
   var lowerCaseAnswer = window.confirm("Would you like to use lowercase letters?");
   if (lowerCaseAnswer)
   {
@@ -67,6 +68,7 @@ function getCharacterTypes ()
     window.alert("You selected no to use special characters");
   }
 
+  // Return Boolean values as an array
   return [lowerCaseAnswer, upperCaseAnswer, numericAnswer, specialCharacterAnswer];
 }
 
@@ -74,13 +76,11 @@ function generatePassword()
 {
   // Get desired number of characters from user
   var charCount = getCharacterCount();
-  console.log(charCount);
 
   // Call getCharacterTypes function to get Boolean values
   var characterTypesAnswers = getCharacterTypes();
-  console.log(characterTypesAnswers);
 
-  // If user does not select any character type
+  // If user does not select any character types
   if ((!characterTypesAnswers[0]) && (!characterTypesAnswers[1]) && (!characterTypesAnswers[2]) && (!characterTypesAnswers[3]))
   {
     window.alert("Need to select at least one character type to generate a password" + "\nPlease try again.");
@@ -90,8 +90,11 @@ function generatePassword()
   {
 
     // Generate the password
+
+    // Empty array to hold password characters
     var passwordArray = [];
 
+    // Generate the number of characters specified by the requested character count
     for (var i = 0; i < charCount; i++)
     {
       // Flag for do while loop
@@ -100,18 +103,23 @@ function generatePassword()
       // Iterate until character is successfully added to passwordArray
       do
       {
-        // Gets random number between 0 and 3
+        // Gets random number between 0 and 3 to select character type
         var characterType = Math.floor(Math.random() * 4)
-        console.log("Character Type: " + characterType);
 
+        // If character type is set to true, generate character
         if (characterTypesAnswers[characterType])
         {
+          // Generate a random number based on the length of the select character type array
           var characterNumber = Math.floor(Math.random() * arrayObject[characterType].length);
+
+          // Use the randomly generated number to select the character to add to the password from the character type array in the array object
           var character = arrayObject[characterType][characterNumber];
 
+          // Add character to the end of the password array
           passwordArray.push(character);
           charFlag = true;
         }
+        // Else randomly select another character type
         else
         {
           continue;
@@ -121,12 +129,8 @@ function generatePassword()
     }
   }
 
-  var password = passwordArray.join("");
-
-  console.log(passwordArray);
-  console.log(password);
-
-  return password;
+  // Return array as a string of characters
+  return passwordArray.join("");
 }
 
 // Assignment Code
@@ -146,8 +150,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // To do
 
-// Remove console.logs
-// Comments
 // README
 // .gitignore
 // deploy
